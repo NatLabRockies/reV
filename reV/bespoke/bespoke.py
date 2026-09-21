49aacf3a98a698a7506faa928e5e6608692c7c49
Paul <pinchukpaul@gmail.com>
2026-09-21 16:56:01 -0600

    pandas update

--- reV/bespoke/bespoke.py
+++ reV/bespoke/bespoke.py
@@ -614,7 +614,7 @@ class BespokeSinglePlant:
                 for annual_ti in self.annual_time_indexes:
                     year = annual_ti.year[0]
                     mask = self.res_df.index.isin(annual_ti)
-                    arr = self.res_df.loc[mask, req].values.flatten()
+                    arr = self.res_df.loc[mask, req].to_numpy().flatten()
                     self._outputs[req + f"-{year}"] = arr
 
             elif req.replace("_mean", "") in self.res_df:
